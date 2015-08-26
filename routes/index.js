d05713c5cc6e3464c060eaf6776e9f37b921a322
Ben Fischer <exofusionx@gmail.com>
2015-08-26 16:07:06 -0500

    Rename View

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if (req.subdomains.indexOf('champion') > -1) {
-    res.render('build_stats', { title: 'champion.builder.gg' });
+    res.render('champion_stats', { title: 'champion.builder.gg' });
   } else {
     res.render('index', { title: 'item.builder.gg' });
   }
