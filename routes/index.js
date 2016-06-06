ab1d6fe49a9cf7ed7bccb6bb1f770be4a44b312a
Ben Fischer <Ben Fischer>
2016-06-05 19:16:49 -0700

    Default to champion builder

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  if (req.subdomains.indexOf('champion') > -1) {
-    res.render('champion_stats', { title: 'champion.builder.gg' });
-  } else {
+  if (req.subdomains.indexOf('item') > -1) {
     res.render('index', { title: 'item.builder.gg' });
+  } else {
+    res.render('champion_stats', { title: 'champion.builder.gg' });
   }
 });
 
